31d710833159c7784449c38b71eaa15a152543cb
Paalan <42642576+paalamugan@users.noreply.github.com>
2021-08-20 18:44:56 +0530

    Update menu-element.ts

--- src/app/models/menu-element.ts
+++ src/app/models/menu-element.ts
@@ -1,15 +1,23 @@
 export interface Chip {
-    value: number, 
-    color: string
+  value: number;
+  color: string;
 }
 
-export interface MenuElement {
-    name: string,
-    icon: string,
-    open?: boolean,
-    chip?: boolean | Chip,
-    link?: string | boolean,
-    tooltip?: string, 
-    sub?: Array<MenuElement>
+export interface Menu {
+  name: string;
+  open?: boolean;
+  chip?: boolean | Chip;
+  link?: string | boolean;
+  tooltip?: string;
+  sub?: Array<MenuElement>;
 }
 
+export interface MenuWithIcon extends Menu {
+  icon: string;
+}
+
+export interface MenuWithSVGIcon extends Menu {
+  svgIcon: string;
+}
+
+export type MenuElement = MenuWithIcon | MenuWithSVGIcon;
