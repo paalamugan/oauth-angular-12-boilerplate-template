4799426e39331b1447cd25e61cab390fa9e38be0
Paalan <42642576+paalamugan@users.noreply.github.com>
2021-09-02 12:33:03 +0530

    Create common.ts

---
+++ src/app/contants/utils/common.ts
@@ -0,0 +1,20 @@
+import { Observable, Subject, defer, delay, of } from "rxjs"
+import { finalize } from "rxjs/operators"
+
+export function delayObservable<T>(data: T): Observable<T> {
+  return of(data).pipe(delay(1500));
+}
+  
+export const prepare = <T>(callback: () => void) => {
+  return (source: Observable<T>): Observable<T> => defer(() => {
+    callback();
+    return source;
+  });
+}
+
+export const indicateLoading = <T>(loading$: Subject<boolean>) => {
+  return (source: Observable<T>): Observable<T> => source.pipe(
+    prepare(() => loading$.next(true)),
+    finalize(() => loading$.next(false))
+  )
+}
