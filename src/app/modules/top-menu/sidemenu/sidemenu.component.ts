6bfb10e21cd1ead373a700d8b3c881383e49a786
Paalan <42642576+paalamugan@users.noreply.github.com>
2021-09-13 17:07:03 +0530

    Automatically open sidebar menu when page is load

--- src/app/modules/top-menu/sidemenu/sidemenu.component.ts
+++ src/app/modules/top-menu/sidemenu/sidemenu.component.ts
@@ -1,24 +1,53 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { map } from 'lodash';
+import { find } from 'lodash';
 import { menus } from '@app/common/sidemenu';
+import { MenuElement } from '@app/models/menu-element';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss'],
 })
+
 export class SidemenuComponent implements OnInit {
   @Input() iconOnly: boolean = false;
-  menus: any;
+  menus!: MenuElement[];
+
   constructor(private router: Router) {}
 
   ngOnInit() {
-    const url = this.router.url;
-    this.menus = map(menus, (menu) => {
-      let link = menu.name.toLowerCase();
-      menu.open = url.includes(link);
-      return menu;
+    this.menus = [...menus];
+    this.getMenus(this.menus);
+  }
+
+  isActiveRoute(link: any) {
+    if (typeof link !== 'string') {
+      return false;
+    }
+    return this.router.url.includes(link);
+  }
+
+  isActiveSubMenu(menu: MenuElement) {
+    if (typeof menu.link === 'string') {
+      return this.isActiveRoute(menu.link);
+    }
+
+    return !!find(menu.sub, (subMenu) => this.isActiveRoute(subMenu.link));
+  }
+
+  getMenus(menus: MenuElement[], menu?: MenuElement) {
+    let previousMenu = menu || ({} as MenuElement);
+    
+    menus.forEach((menu: any) => {
+      if (menu?.sub?.length) {
+        menu.open = !!this.isActiveSubMenu(menu);
+        previousMenu = this.getMenus(menu.sub, menu);
+        menu.open = previousMenu.open || menu.open;
+      }
     });
+    
+    return previousMenu;
   }
 }
+
